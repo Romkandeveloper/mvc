cbf79e83dbacca04687c70d4b2777610f4a8cafb
Romkandeveloper <57558370+Romkandeveloper@users.noreply.github.com>
2022-05-31 18:41:29 +0300

    Delete .env.php

--- .env.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'MAP_API_KEY' => 'AIzaSyA2KuPasVz8yDJbNmtCoYaH7y8dkDhA45c',
-    'DB_HOST' => 'localhost',
-    'DB_NAME' => 'conferences',
-    'DB_PASSWORD' => '',
-    'DB_USER' => 'root'
-];
